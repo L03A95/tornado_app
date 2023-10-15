489a273ecc00efce1ef3f9bdb71c0fad6601f5e5
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-10-15 17:37:16 -0300

    Added state to save the Menu

--- src/components/CreateForm.tsx
+++ src/components/CreateForm.tsx
@@ -1,23 +1,37 @@
-import { View, TextInput, Text, } from 'react-native'
+import { View, TextInput, Text, Button, } from 'react-native'
 import { Picker } from "@react-native-picker/picker";
-
+import { useState } from 'react';
 
 
 export default function CreateForm() {
 
+    const [newMenu, setNewMenu] = useState({
+        name: '',
+        description: '',
+        price: 0,
+        image: '',
+        category: 'entrada',
+        active: true
+    })
+
+    const handleInputChange = (type : string , value : string | number) => {
+        setNewMenu({...newMenu, [type]: value})
+    }
+
     return (
         <View>
-            <TextInput placeholder='Nombre del plato'></TextInput>
-            <TextInput placeholder='Descripción'></TextInput>
-
-            <TextInput placeholder='Precio' keyboardType="numeric"></TextInput>
-            <Picker>
+            <Text>Añadir Menú</Text>
+            <TextInput placeholder='Nombre' onChangeText={(text) => { handleInputChange('name', text)}}></TextInput>
+            <TextInput placeholder='Descripción' onChangeText={(text) => { handleInputChange('description', text)}}></TextInput>
+            <TextInput placeholder='Precio' keyboardType="numeric" onChangeText={(text) => { handleInputChange('price', text)}}></TextInput>
+            <Picker selectedValue={newMenu.category} onValueChange={(value) => handleInputChange('category', value)}>
                 <Picker.Item label="Entrada" value='entrada'/>
                 <Picker.Item label="Parrilla" value='parrilla'/>
                 <Picker.Item label="Cafetería" value='cafetería'/>
                 <Picker.Item label="Dulces" value='dulces'/>
                 <Picker.Item label="Bebidas" value='Bebidas'/>
             </Picker>
+            <Button onPress={() => console.log(newMenu)} title='Consologear'></Button>
         </View>
     )
 }
