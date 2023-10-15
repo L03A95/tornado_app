1e8fabb8b2f78477b44b60ad4bcdcfd5a20ace48
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-10-15 18:06:50 -0300

    Implemented image-picker library

--- src/components/CreateForm.tsx
+++ src/components/CreateForm.tsx
@@ -1,6 +1,7 @@
-import { View, TextInput, Text, Button, } from 'react-native'
+import { View, TextInput, Text, Button, Image } from 'react-native'
 import { Picker } from "@react-native-picker/picker";
 import { useState } from 'react';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 
 export default function CreateForm() {
@@ -18,12 +19,22 @@ export default function CreateForm() {
         setNewMenu({...newMenu, [type]: value})
     }
 
+    const handleImageChange = async () => {
+
+        launchImageLibrary({mediaType: 'photo', }, (response) => {
+            if (!response.didCancel && response.assets) {
+              setNewMenu({ ...newMenu, image: response.assets[0].uri ? response.assets[0].uri : 'undefined' });
+            }
+          });
+    }
+
     return (
         <View>
             <Text>Añadir Menú</Text>
             <TextInput placeholder='Nombre' onChangeText={(text) => { handleInputChange('name', text)}}></TextInput>
             <TextInput placeholder='Descripción' onChangeText={(text) => { handleInputChange('description', text)}}></TextInput>
             <TextInput placeholder='Precio' keyboardType="numeric" onChangeText={(text) => { handleInputChange('price', text)}}></TextInput>
+            <Button title='Lunch camera' onPress={async () => handleImageChange()}></Button>
             <Picker selectedValue={newMenu.category} onValueChange={(value) => handleInputChange('category', value)}>
                 <Picker.Item label="Entrada" value='entrada'/>
                 <Picker.Item label="Parrilla" value='parrilla'/>
@@ -31,6 +42,7 @@ export default function CreateForm() {
                 <Picker.Item label="Dulces" value='dulces'/>
                 <Picker.Item label="Bebidas" value='Bebidas'/>
             </Picker>
+            {newMenu.image && <Image source={{ uri: newMenu.image }} style={{ width: 200, height: 200 }} />}
             <Button onPress={() => console.log(newMenu)} title='Consologear'></Button>
         </View>
     )
