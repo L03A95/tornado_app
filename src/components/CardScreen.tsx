fdc887ceebf5ca12d40b174d9a35c085641ffa6b
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-10-19 23:41:43 -0300

    Edited styles from CardScreen

--- src/components/CardScreen.tsx
+++ src/components/CardScreen.tsx
@@ -15,7 +15,7 @@ export default function CardScreen ({navigation} : {navigation: any}) {
         image: 'https://png.pngtree.com/thumb_back/fh260/background/20210207/pngtree-gray-solid-color-simple-background-image_557028.jpg',
         price: 0,
         category: '',
-        active: false
+        active: ''
     })
 
     useEffect(() => {
@@ -25,33 +25,55 @@ export default function CardScreen ({navigation} : {navigation: any}) {
     },[])
 
     return (
-        <View>
-            <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.button}>
-                <Image source={logo} style={styles.image}/>
+        <View style={styles.background}>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')} >
+                <Image source={logo} style={styles.back_btn}/>
             </TouchableOpacity>
-            <Text>{menu.name}</Text>
-            <Text>{menu.description}</Text>
-            <Image source={{uri: menu.image}} style={{height: 80, width: 140}}></Image>
-            <Text></Text>
-            <Text></Text>
-            <Text></Text>
+            <View style={styles.content_wrapper}>
+                <View style={styles.text_wrapper}>
+                    <Text>Nombre del Menú</Text>
+                    <Text style={styles.text}>{menu.name}</Text>
+                </View>
+
+                <Text style={styles.text}>{menu.description}</Text>
+                <Image source={{uri: menu.image}} style={{height: 180, width: 300}}></Image>
+                <Text style={styles.text}>${menu.price}</Text>
+                <Text>{menu.category}</Text>
+                <Text>{menu.active ? 'Activo' : 'Inactivo'}</Text>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    image: {
+    background: {
+        backgroundColor: '#fff0f0',
+        flex: 1,
+    },
+    back_btn: {
         height: 40,
         width: 40,
         margin: 5,
         backgroundColor: '#c00',
         borderRadius: 15,
-        marginRight: 20
-    },
-    button: {
-        
-    },
-    background: {
+        marginRight: 20,
 
+    },
+    content_wrapper: {
+        alignItems: 'center'
+    },
+    text: {
+        backgroundColor: '#ecc',
+        margin: 10,
+        borderRadius: 5,
+        paddingLeft: 15,
+        fontSize: 16,
+        width: '90%',
+        color: '#800',
+        padding: 14
+    },
+    text_wrapper: {
+        width: '100%',
+        marginVertical: 10
     }
 })
